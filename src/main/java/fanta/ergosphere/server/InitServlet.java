7a1b70c079fd2cde081afe4d2e6473af36304e95
jellymlg <erikpanta@gmail.com>
2024-04-18 21:14:24 +0200

    Update dependencies, minize JAR

--- src/main/java/fanta/ergosphere/server/InitServlet.java
+++ src/main/java/fanta/ergosphere/server/InitServlet.java
@@ -10,10 +10,10 @@ import fanta.ergosphere.util.SwayDB.Table;
 
 import java.io.IOException;
 import java.io.InputStream;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/fanta/ergosphere/server/StatusServlet.java
+++ src/main/java/fanta/ergosphere/server/StatusServlet.java
@@ -1,10 +1,10 @@
 package fanta.ergosphere.server;
 
 import fanta.ergosphere.process.ProcessMonitor;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public final class StatusServlet extends HttpServlet {
